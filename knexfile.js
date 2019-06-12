b7eca9dee6e37540d850b05d2d604b02c45c6cb9
Leonel <leonelaf12@gmail.com>
2019-06-12 16:38:12 -0400

    auth router working properly

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/newdb.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+
+  },
+  
+};
